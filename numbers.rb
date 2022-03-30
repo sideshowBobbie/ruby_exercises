a3e65232de7b21739f4d3c99ca773643ab2780b5
sideshowBobbie <o.bobbie.carr@gmail.com>
2022-03-29 22:48:47 -0500

    numbers exercise

---
+++ numbers.rb
@@ -0,0 +1,32 @@
+count = 1
+
+# 10.times do
+#   puts count
+#   count += 1
+# end
+
+# while count <= 5
+#   puts count
+#   count += 1
+# end
+
+#if answer != 5 #if ; answer is not 5
+# if result != true ; #if result does not equal true ; if it is fale
+# if !(result == true) #same as above
+# unless result == true
+# if !(result == true)
+
+array = [12,7,9] #ref ln 29: array returns true because it exists
+
+# if !(array.include?(5))
+# unless array.include?(5)
+
+result = nil
+
+unless result
+  puts "hEelo!!!"
+end
+
+if array #as long as it exists it proves true
+  puts "hey that's a thing"
+end
